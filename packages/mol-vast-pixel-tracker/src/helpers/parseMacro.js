952923bb03f713678fb8bc8f3748e7f2b1766e2e
Carlos <carlos.pastor@mailonline.co.uk>
2017-08-24 11:24:10 +0100

    feat: percent-encode macro variables and add
    TIMESTAMP default variable
    affects: mol-vast-pixel-tracker

--- packages/mol-vast-pixel-tracker/src/helpers/parseMacro.js
+++ packages/mol-vast-pixel-tracker/src/helpers/parseMacro.js
@@ -1,6 +1,7 @@
 /**
  * Parses the passed macro with the passed data and returns the resulting parsed Macro.
  * If no CACHEBUSTING property is passed in the data it will generate a random one on its own.
+ * If no TIMESTAMP property is passed in the data it will generate a one on its own.
  *
  * @param {string} macro - The string macro to be parsed.
  * @param {Object} data - The data used by the macro.
@@ -14,8 +15,12 @@ const parseMacro = (macro, data) => {
     data.CACHEBUSTING = Math.round(Math.random() * 1.0e+10);
   }
 
+  if (!Boolean(data.TIMESTAMP)) {
+    data.TIMESTAMP = new Date().toISOString();
+  }
+
   Object.keys(data).forEach((key) => {
-    const value = data[key];
+    const value = encodeURIComponent(data[key]);
 
     parsedMacro = parsedMacro.replace(new RegExp('\\[' + key + '\\]', 'gm'), value);
   });
