e0a42776be5be5fae0a8a829ab71ee4b033b5c1a
Carlos <carlos.pastor@mailonline.co.uk>
2017-09-28 13:31:24 +0100

    test: fix failing tests due to fixtures refactor
    affects: mol-vast-pixel-tracker

--- packages/mol-vast-pixel-tracker/test/helpers/parseMacro.spec.js
+++ packages/mol-vast-pixel-tracker/test/helpers/parseMacro.spec.js
@@ -2,50 +2,50 @@ import MockDate from 'mockdate';
 import parseMacro from '../../src/helpers/parseMacro';
 
 test('parseMacro must replace the passed variables in the macro', () => {
-  const macro = 'http://test.example.com/[ERRORCODE]/[BLA]';
+  const macro = 'https://test.example.com/[ERRORCODE]/[BLA]';
 
   expect(parseMacro(macro, {})).toBe(macro);
-  expect(parseMacro(macro, {ERRORCODE: '101'})).toBe('http://test.example.com/101/[BLA]');
+  expect(parseMacro(macro, {ERRORCODE: '101'})).toBe('https://test.example.com/101/[BLA]');
   expect(parseMacro(macro, {
     BLA: 'bla',
     ERRORCODE: '101'
-  })).toBe('http://test.example.com/101/bla');
+  })).toBe('https://test.example.com/101/bla');
 });
 
 test('parseMacro must fill the CACHEBUSTING variable if not provided', () => {
-  const macro = 'http://test.example.com/[CACHEBUSTING]';
+  const macro = 'https://test.example.com/[CACHEBUSTING]';
 
   expect(parseMacro(macro, {})).not.toBe(macro);
-  expect(parseMacro(macro, {})).toEqual(expect.stringMatching(/http:\/\/test.example.com\/\d+/));
+  expect(parseMacro(macro, {})).toEqual(expect.stringMatching(/https:\/\/test.example.com\/\d+/));
 });
 
 test('parseMacro must not fill the CACHEBUSTING variable if provided', () => {
-  const macro = 'http://test.example.com/[CACHEBUSTING]';
+  const macro = 'https://test.example.com/[CACHEBUSTING]';
 
-  expect(parseMacro(macro, {CACHEBUSTING: 'foo'})).toBe('http://test.example.com/foo');
+  expect(parseMacro(macro, {CACHEBUSTING: 'foo'})).toBe('https://test.example.com/foo');
 });
 
 test('parseMacro must percent-encode the replaced macros', () => {
-  const macro = 'http://test.example.com/[URL]';
+  const macro = 'https://test.example.com/[URL]';
 
-  expect(parseMacro(macro, {URL: 'http://someExample.com'})).toBe('http://test.example.com/http%3A%2F%2FsomeExample.com');
+  expect(parseMacro(macro, {URL: 'http://someExample.com'})).toBe('https://test.example.com/http%3A%2F%2FsomeExample.com');
 });
 
 test('parseMacro must fill the TIMESTAMP variable if not provided', () => {
   const now = new Date();
 
   MockDate.set(now);
-  const macro = 'http://test.example.com/[TIMESTAMP]';
+  const macro = 'https://test.example.com/[TIMESTAMP]';
 
   global.Date.now = jest.fn(() => now);
 
-  expect(parseMacro(macro, {})).toEqual(`http://test.example.com/${encodeURIComponent(now.toISOString())}`);
+  expect(parseMacro(macro, {})).toEqual(`https://test.example.com/${encodeURIComponent(now.toISOString())}`);
 
   MockDate.reset();
 });
 
 test('parseMacro must not provide the TIMESTAMP variable if provided', () => {
-  const macro = 'http://test.example.com/[TIMESTAMP]';
+  const macro = 'https://test.example.com/[TIMESTAMP]';
 
-  expect(parseMacro(macro, {TIMESTAMP: 'today'})).toBe('http://test.example.com/today');
+  expect(parseMacro(macro, {TIMESTAMP: 'today'})).toBe('https://test.example.com/today');
 });
