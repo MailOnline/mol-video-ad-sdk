5e51bc1b1f54634259f7b8f123f6090cb63e0ee7
Carlos <carlos.pastor@mailonline.co.uk>
2017-09-29 10:54:38 +0100

    feat: inital work on vast ad unit class
    Created the class with an interface proposal (not final yet). Added some
    helper methods and tested them.
    affects: mol-vast-ad-unit

--- packages/mol-vast-ad-unit/src/VastAdUnit.js
+++ packages/mol-vast-ad-unit/src/VastAdUnit.js
@@ -1,7 +1,77 @@
+/* eslint-disable class-methods-use-this */
 import Emitter from 'mol-tiny-emitter';
+import {getMediaFiles} from 'mol-vast-selectors';
+import canPlay from './helpers/canPlay';
+import sortMediaByBestFit from './helpers/sortMediaByBestFit';
+
+const findBestMedia = (videoElement, mediaFiles, container) => {
+  const screenRect = container.getBoundingClientRect();
+  const suportedMediaFiles = mediaFiles.filter((mediaFile) => canPlay(videoElement, mediaFile));
+  const sortedMediaFiles = sortMediaByBestFit(suportedMediaFiles, screenRect);
+
+  return sortedMediaFiles[0];
+};
+
+const onErrorCallbacks = Symbol('onErrorCallbacks');
+const onCompleteCallbacks = Symbol('onCompleteCallbacks');
 
 class VastAdUnit extends Emitter {
-  // Empty for the moment
+  constructor (vastAdChain, videoAdContainer, {logger = console} = {}) {
+    super(logger);
+
+    this.vastAdChain = vastAdChain;
+    this.videoAdContainer = videoAdContainer;
+    this.error = null;
+    this.errorCode = null;
+    this.assetUri = null;
+    this.contentplayhead = null;
+    this[onErrorCallbacks] = [];
+    this[onCompleteCallbacks] = [];
+  }
+
+  run () {
+    const videoElement = this.videoAdContainer.videoElement;
+    const mediaFiles = getMediaFiles(this.vastAdChain[0].ad) || [];
+    const media = findBestMedia(videoElement, mediaFiles, this.videoAdContainer.element);
+
+    if (!Boolean(media)) {
+      throw new Error('Can\'t find a suitable media to play');
+    }
+
+    videoElement.src = media.src;
+
+    // TODO: listen to media events and emit tracking events
+    // TODO: add the symbol to the container
+
+    videoElement.play();
+  }
+
+  cancel () {
+    const videoElement = this.videoAdContainer.videoElement;
+
+    videoElement.pause();
+    this.destroy();
+  }
+
+  // eslint-disable-next-line promise/prefer-await-to-callbacks
+  oncomplete (callback) {
+    if (typeof callback === 'function') {
+      this[onCompleteCallbacks].push(callback);
+    }
+  }
+
+  // eslint-disable-next-line promise/prefer-await-to-callbacks
+  onerror (callback) {
+    if (typeof callback === 'function') {
+      this[onErrorCallbacks].push(callback);
+    }
+  }
+
+  destroy () {
+    // removes the ad source from the video element
+    // stop listening to media events
+    // removes the symbol from the ad container
+  }
 }
 
 export default VastAdUnit;
---
+++ packages/mol-vast-ad-unit/src/helpers/canPlay.js
@@ -0,0 +1,12 @@
+import guessMimeType from './guessMimeType';
+
+const canPlay = (videoElement, mediaFile) => {
+  const {
+    src,
+    type
+  } = mediaFile;
+
+  return videoElement.canPlayType(type || guessMimeType(src));
+};
+
+export default canPlay;
