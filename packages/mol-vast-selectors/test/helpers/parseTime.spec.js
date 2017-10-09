5a02762294b71ce14dccaa57498265bdd3ec6830
Carlos <carlos.pastor@mailonline.co.uk>
2017-10-09 17:21:15 +0100

    test: test parseTime helper
    affects: mol-vast-selectors

---
+++ packages/mol-vast-selectors/test/helpers/parseTime.spec.js
@@ -0,0 +1,14 @@
+import parseTime from '../../src/helpers/parseTime';
+
+test('parseTime must return the passed offset string in ms', () => {
+  expect(parseTime('00:00:05.000')).toBe(5000);
+  expect(parseTime('01:03:05.000')).toBe(3785000);
+  expect(parseTime('00:00:05.050')).toBe(5050);
+});
+
+test('parseTime must return null if you don\'t pass a proper offset', () => {
+  expect(parseTime('')).toEqual(null);
+  expect(parseTime('23423452353')).toEqual(null);
+  expect(parseTime()).toEqual(null);
+  expect(parseTime(undefined)).toEqual(null);
+});
