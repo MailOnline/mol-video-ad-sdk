5d7b42b682bdd0c50d125bec0968f31ab7864ddc
Carlos <carlos.pastor@mailonline.co.uk>
2017-10-16 16:49:54 +0100

    feat: surround loadIcon logic with try catch
    and test logic
    affects: mol-video-ad-unit

--- packages/mol-video-ad-unit/src/helpers/icons/loadIcon.js
+++ packages/mol-video-ad-unit/src/helpers/icons/loadIcon.js
@@ -1,18 +1,27 @@
 import createResource from '../resources/createResource';
 
-const loadIcon = (payload, {document, placeholder}) => new Promise((resolve, reject) => {
-  const iconElement = createResource(document, payload);
+const loadIcon = (icon, {document, placeholder}) => new Promise((resolve, reject) => {
+  try {
+    const iconElement = createResource(document, icon);
 
-  iconElement.addEventListener('error', reject);
-  iconElement.addEventListener('load', () => {
-    placeholder.removeChild(iconElement);
-    iconElement.style.zIndex = 0;
-    resolve(iconElement);
-  });
+    iconElement.addEventListener('error', () => {
+      placeholder.removeChild(iconElement);
+      iconElement.style.zIndex = 0;
+      reject(new Error('Error loading icon'));
+    });
 
-  // Some browsers will not load the resource if they are not added to the DOM
-  iconElement.style.zIndex = -9999;
-  placeholder.appendChild(iconElement);
+    iconElement.addEventListener('load', () => {
+      placeholder.removeChild(iconElement);
+      iconElement.style.zIndex = 0;
+      resolve(iconElement);
+    });
+
+    // Some browsers will not load the resource if they are not added to the DOM
+    iconElement.style.zIndex = -9999;
+    placeholder.appendChild(iconElement);
+  } catch (error) {
+    reject(error);
+  }
 });
 
 export default loadIcon;
