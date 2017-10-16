38752578d9aa81df43465f8977d2dd693235011a
Carlos <carlos.pastor@mailonline.co.uk>
2017-10-16 15:59:19 +0100

    refactor: remove unused helper
    affects: mol-video-ad-unit

--- packages/mol-video-ad-unit/src/helpers/utils/reflect.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable object-property-newline, promise/prefer-await-to-then */
-const reflect = (promise) => promise
-  .then((value) => ({status: 'resolved', value}))
-  .catch((error) => ({error, status: 'rejected'}));
-
-export default reflect;
