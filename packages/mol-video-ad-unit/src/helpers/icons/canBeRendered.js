2c9b16bf6515460cb9d073d3d8ba85d52ecd6925
Carlos <carlos.pastor@mailonline.co.uk>
2017-10-19 09:16:46 +0100

    fix: fix canBeRendered logic and test it
    affects: mol-video-ad-unit

--- packages/mol-video-ad-unit/src/helpers/icons/canBeRendered.js
+++ packages/mol-video-ad-unit/src/helpers/icons/canBeRendered.js
@@ -1,45 +1,44 @@
 /* eslint-disable no-shadow */
-const calculateArea = (element) => {
-  const {
-    height,
-    width
-  } = element.getBoundingClientRect();
-
-  return height * width;
-};
+const calculateArea = ({height, width}) => height * width;
 
 export const hasSpace = (newIcon, config) => {
   const {
     drawnIcons,
     placeholder
   } = config;
-  const placeholderArea = calculateArea(placeholder);
-  const iconArea = calculateArea(newIcon.element);
-  const usedIconsArea = drawnIcons.reduce((accumulator, icon) => accumulator + icon.element, 0);
+  const placeholderArea = calculateArea(placeholder.getBoundingClientRect());
+  const iconArea = calculateArea(newIcon);
+  const usedIconsArea = drawnIcons.reduce((accumulator, icon) => accumulator + calculateArea(icon), 0);
 
   return iconArea + usedIconsArea <= placeholderArea * 0.35;
 };
 
-export const withinBoundaries = (newIcon) => {
-  const {
-    left,
-    top
-  } = newIcon;
+export const withinBoundaries = (newIcon, {placeholder}) => {
+  const phRect = placeholder.getBoundingClientRect();
 
-  return left >= 0 && top >= 0;
+  return newIcon.left >= 0 &&
+    newIcon.left + newIcon.width <= phRect.width &&
+    newIcon.top >= 0 &&
+    newIcon.top + newIcon.height <= phRect.height;
 };
 
 const right = ({left, width}) => left + width;
 const left = ({left}) => left;
 const top = ({top}) => top;
 const bottom = ({top, height}) => top + height;
-const overlap = (newIcon, drawnIcon) => left(newIcon) > right(drawnIcon) ||
-  right(newIcon) < left(drawnIcon) ||
-  bottom(newIcon) < top(drawnIcon) ||
-  top(newIcon) > bottom(drawnIcon);
+const overlap = (newIcon, drawnIcon) => {
+  if (left(newIcon) > right(drawnIcon) ||
+    right(newIcon) < left(drawnIcon) ||
+    bottom(newIcon) < top(drawnIcon) ||
+    top(newIcon) > bottom(drawnIcon)) {
+    return false;
+  }
+
+  return true;
+};
 
 export const withoutOverlaps = (newIcon, {drawnIcons}) => !drawnIcons.some((drawnIcon) => overlap(newIcon, drawnIcon));
 
-const canBeRendered = (newIcon, config) => hasSpace(newIcon, config) && withinBoundaries(newIcon) && withoutOverlaps(newIcon, config);
+const canBeRendered = (newIcon, config) => hasSpace(newIcon, config) && withinBoundaries(newIcon, config) && withoutOverlaps(newIcon, config);
 
 export default canBeRendered;
