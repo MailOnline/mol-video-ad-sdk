66c5b2d3b928e69b41f7ec9270dd3dd57574fb8c
Carlos <carlos.pastor@mailonline.co.uk>
2017-10-26 14:28:08 +0100

    fix: fix addIcons logic and test it
    affects: mol-video-ad-unit

--- packages/mol-video-ad-unit/src/helpers/icons/addIcons.js
+++ packages/mol-video-ad-unit/src/helpers/icons/addIcons.js
@@ -1,26 +1,23 @@
 import once from '../dom/once';
 import renderIcons from './renderIcons';
 
-const canBeAdded = ({offset, duration}, currentTimeInMs) => {
-  if (Boolean(offset) && offset > currentTimeInMs || Boolean(duration) && currentTimeInMs - offset > duration) {
-    return false;
-  }
+const canBeAdded = (icon, videoElement) => {
+  const currentTimeInMs = videoElement.currentTime * 1000;
+  const videoDurationInMs = videoElement.duration * 1000;
+  const offset = icon.offset || 0;
+  const duration = icon.duration || videoDurationInMs;
 
-  return true;
+  return offset <= currentTimeInMs && currentTimeInMs - offset <= duration;
 };
 
-const hasToRedraw = (icons, currentTimeInMs, adDurationInMs) => icons.some(({duration, offset}) =>
-  canBeAdded({
-    duration: duration || adDurationInMs,
-    offset: offset || 0
-  }, currentTimeInMs));
+const hasToRedraw = (icons, videoElement) => icons.some((icon) => canBeAdded(icon, videoElement));
 
 const removeDrawnIcons = (icons) => icons
   .filter(({element}) => Boolean(element) && Boolean(element.parentNode))
   .forEach(({element}) => element.parentNode.removeChild(element));
 
 const addIcons = (icons, {videoAdContainer, ...rest}) => {
-  const {videoElement} = videoAdContainer;
+  const {videoElement, element} = videoAdContainer;
   let finished = false;
 
   const addIconsToAd = async () => {
@@ -30,23 +27,23 @@ const addIcons = (icons, {videoAdContainer, ...rest}) => {
       return;
     }
 
-    const durationInMs = videoElement.duration * 1000;
-    const currentTimeInMs = videoElement.currentTime * 1000;
-    const iconsToDraw = icons.filter((icon) => canBeAdded(icon, currentTimeInMs));
+    const iconsToDraw = icons.filter((icon) => canBeAdded(icon, videoElement));
 
     await renderIcons(iconsToDraw, {
       videoAdContainer,
       ...rest
     });
 
+    element.dispatchEvent(new Event('iconsdrawn'));
+
     if (finished) {
-      removeDrawnIcons();
+      removeDrawnIcons(icons);
 
       return;
     }
 
-    if (hasToRedraw(icons, currentTimeInMs, durationInMs)) {
-      once('timeupdate', addIconsToAd);
+    if (hasToRedraw(icons, videoElement)) {
+      once(videoElement, 'timeupdate', addIconsToAd);
     }
   };
 
@@ -54,7 +51,7 @@ const addIcons = (icons, {videoAdContainer, ...rest}) => {
 
   return () => {
     finished = true;
-    removeDrawnIcons();
+    removeDrawnIcons(icons);
   };
 };
 
---
+++ packages/mol-video-ad-unit/test/helpers/icons/addIcons.spec.js
@@ -0,0 +1,263 @@
+import {createVideoAdContainer} from 'mol-video-ad-container';
+import addIcons from '../../../src/helpers/icons/addIcons';
+import createResource from '../../../src/helpers/resources/createResource';
+import canBeRendered from '../../../src/helpers/icons/canBeRendered';
+
+jest.mock('../../../src/helpers/resources/createResource');
+jest.mock('../../../src/helpers/icons/canBeRendered');
+
+const waitFor = (element, eventName) => new Promise((resolve) => element.addEventListener(eventName, resolve));
+
+let videoAdContainer;
+let logger;
+
+beforeEach(async () => {
+  videoAdContainer = await createVideoAdContainer(document.createElement('DIV'));
+  logger = {error: jest.fn()};
+  const {videoElement} = videoAdContainer;
+
+  Object.defineProperty(videoElement, 'currentTime', {
+    value: 0,
+    writable: true
+  });
+
+  Object.defineProperty(videoElement, 'duration', {
+    value: 10,
+    writable: true
+  });
+
+  createResource.mockImplementation(() => {
+    const resourceElement = document.createElement('IMG');
+
+    resourceElement.classList.add('mock-icon-element');
+
+    resourceElement.width = 100;
+    resourceElement.height = 100;
+
+    setTimeout(() => resourceElement.dispatchEvent(new Event('load')));
+
+    return resourceElement;
+  });
+
+  canBeRendered.mockImplementation(() => true);
+});
+
+afterEach(() => {
+  videoAdContainer = null;
+  logger = null;
+});
+
+test('addIcons must add the icons to the video ad container', async () => {
+  const icons = [
+    {
+      height: 20,
+      width: 20,
+      xPosition: 'right',
+      yPosition: 'top'
+    },
+    {
+      height: 20,
+      width: 20,
+      xPosition: 'left',
+      yPosition: 'top'
+    }
+  ];
+  const {element} = videoAdContainer;
+
+  addIcons(icons, {
+    logger,
+    videoAdContainer
+  });
+
+  await waitFor(element, 'iconsdrawn');
+
+  icons.forEach((icon) => expect(element.contains(icon.element)).toBe(true));
+});
+
+test('addIcons must not the the icons whose offset is not meet yet', async () => {
+  const icons = [
+    {
+      height: 20,
+      offset: 5000,
+      width: 20,
+      xPosition: 'right',
+      yPosition: 'top'
+    },
+    {
+      height: 20,
+      width: 20,
+      xPosition: 'left',
+      yPosition: 'top'
+    }
+  ];
+  const {element, videoElement} = videoAdContainer;
+
+  addIcons(icons, {
+    logger,
+    videoAdContainer
+  });
+
+  await waitFor(element, 'iconsdrawn');
+
+  expect(element.contains(icons[0].element)).toBe(false);
+  expect(element.contains(icons[1].element)).toBe(true);
+
+  videoElement.currentTime = 5;
+
+  videoElement.dispatchEvent(new Event('timeupdate'));
+
+  await waitFor(element, 'iconsdrawn');
+
+  expect(element.contains(icons[0].element)).toBe(true);
+  expect(element.contains(icons[1].element)).toBe(true);
+});
+
+test('addIcons must remove the already drawn icons before a redraw', async () => {
+  const icons = [
+    {
+      height: 20,
+      width: 20,
+      xPosition: 'right',
+      yPosition: 'top'
+    },
+    {
+      height: 20,
+      width: 20,
+      xPosition: 'left',
+      yPosition: 'top'
+    }
+  ];
+  const {element, videoElement} = videoAdContainer;
+
+  addIcons(icons, {
+    logger,
+    videoAdContainer
+  });
+
+  await waitFor(element, 'iconsdrawn');
+
+  expect(element.contains(icons[0].element)).toBe(true);
+  expect(element.contains(icons[1].element)).toBe(true);
+
+  videoElement.currentTime = 5;
+
+  videoElement.dispatchEvent(new Event('timeupdate'));
+
+  expect(element.contains(icons[0].element)).toBe(false);
+  expect(element.contains(icons[1].element)).toBe(false);
+
+  await waitFor(element, 'iconsdrawn');
+
+  expect(element.contains(icons[0].element)).toBe(true);
+  expect(element.contains(icons[1].element)).toBe(true);
+});
+
+test('addIcons must remove the icons once the duration is met', async () => {
+  const icons = [
+    {
+      duration: 5000,
+      height: 20,
+      width: 20,
+      xPosition: 'right',
+      yPosition: 'top'
+    },
+    {
+      height: 20,
+      width: 20,
+      xPosition: 'left',
+      yPosition: 'top'
+    }
+  ];
+  const {element, videoElement} = videoAdContainer;
+
+  addIcons(icons, {
+    logger,
+    videoAdContainer
+  });
+
+  await waitFor(element, 'iconsdrawn');
+
+  expect(element.contains(icons[0].element)).toBe(true);
+  expect(element.contains(icons[1].element)).toBe(true);
+
+  videoElement.currentTime = 5.1;
+
+  videoElement.dispatchEvent(new Event('timeupdate'));
+
+  await waitFor(element, 'iconsdrawn');
+
+  expect(element.contains(icons[0].element)).toBe(false);
+  expect(element.contains(icons[1].element)).toBe(true);
+});
+
+test('addIcons must return a remove function', async () => {
+  const icons = [
+    {
+      height: 20,
+      width: 20,
+      xPosition: 'right',
+      yPosition: 'top'
+    },
+    {
+      height: 20,
+      width: 20,
+      xPosition: 'left',
+      yPosition: 'top'
+    }
+  ];
+  const {element} = videoAdContainer;
+
+  const removeIcons = addIcons(icons, {
+    logger,
+    videoAdContainer
+  });
+
+  await waitFor(element, 'iconsdrawn');
+
+  expect(element.contains(icons[0].element)).toBe(true);
+  expect(element.contains(icons[1].element)).toBe(true);
+
+  removeIcons();
+
+  expect(element.contains(icons[0].element)).toBe(false);
+  expect(element.contains(icons[1].element)).toBe(false);
+});
+
+test('addIcons on remove must remove the icons on corner cases too', async () => {
+  const icons = [
+    {
+      height: 20,
+      width: 20,
+      xPosition: 'right',
+      yPosition: 'top'
+    },
+    {
+      height: 20,
+      width: 20,
+      xPosition: 'left',
+      yPosition: 'top'
+    }
+  ];
+  const {element, videoElement} = videoAdContainer;
+
+  const removeIcons = addIcons(icons, {
+    logger,
+    videoAdContainer
+  });
+
+  await waitFor(element, 'iconsdrawn');
+
+  expect(element.contains(icons[0].element)).toBe(true);
+  expect(element.contains(icons[1].element)).toBe(true);
+
+  videoElement.currentTime = 6;
+
+  videoElement.dispatchEvent(new Event('timeupdate'));
+
+  removeIcons();
+
+  await waitFor(element, 'iconsdrawn');
+
+  expect(element.contains(icons[0].element)).toBe(false);
+  expect(element.contains(icons[1].element)).toBe(false);
+});
