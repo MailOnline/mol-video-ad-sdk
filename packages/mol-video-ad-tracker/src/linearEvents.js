8ca8896c85922c5b73f43bf00e91836d2511788f
Carlos <carlos.pastor@mailonline.co.uk>
2017-11-02 09:55:39 +0000

    feat: add linearEvents to video-ad-tracker
    affects: mol-video-ad-tracker

---
+++ packages/mol-video-ad-tracker/src/linearEvents.js
@@ -0,0 +1,48 @@
+export const acceptInvitationLinear = 'acceptInvitationLinear';
+export const complete = 'complete';
+export const clickThrough = 'clickThrough';
+export const error = 'error';
+export const firstQuartile = 'firstQuartile';
+export const impression = 'impression';
+export const midpoint = 'midpoint';
+export const mute = 'mute';
+export const otherAdInteraction = 'otherAdInteraction';
+export const pause = 'pause';
+export const playerCollapse = 'playerCollapse';
+export const playerExpand = 'playerExpand';
+export const progress = 'progress';
+export const resume = 'resume';
+export const rewind = 'rewind';
+export const skip = 'skip';
+export const start = 'start';
+export const thirdQuartile = 'thirdQuartile';
+export const timeSpentViewing = 'timeSpentViewing';
+export const unmute = 'unmute';
+export const iconClick = 'iconClick';
+export const iconView = 'iconView';
+
+export default {
+  acceptInvitationLinear,
+  clickThrough,
+  complete,
+  error,
+  firstQuartile,
+  iconClick,
+  iconView,
+  impression,
+  midpoint,
+  mute,
+  otherAdInteraction,
+  pause,
+  playerCollapse,
+  playerExpand,
+  progress,
+  resume,
+  rewind,
+  skip,
+  start,
+  thirdQuartile,
+  timeSpentViewing,
+  unmute
+};
+
