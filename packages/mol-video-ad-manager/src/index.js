1526ab7e21946b81eaa07c894bf6eb153302cb00
Carlos <carlos.pastor@mailonline.co.uk>
2017-11-07 16:51:04 +0000

    feat: add createVideoAdContainer to videoAdManager
    ... namespace
    affects: mol-video-ad-manager

--- packages/mol-video-ad-manager/src/index.js
+++ packages/mol-video-ad-manager/src/index.js
@@ -1,8 +1,13 @@
+import {createVideoAdContainer} from 'mol-video-ad-container';
 import load from './load';
 import loadNext from './loadNext';
 import isAdPod from './isAdPod';
 
+/**
+ * @namespace MolVideoAdManager
+ */
 export {
+  createVideoAdContainer,
   isAdPod,
   load,
   loadNext
--- packages/mol-video-ad-manager/test/index.spec.js
+++ packages/mol-video-ad-manager/test/index.spec.js
@@ -1,18 +1,23 @@
 import {
+  createVideoAdContainer,
   isAdPod,
   load,
   loadNext
 } from '../src/index';
 
-test('mol-video-ad-manager must publish the load function', () => {
+test('mol-video-ad-manager must publish `load`', () => {
   expect(load).toBeInstanceOf(Function);
 });
 
-test('mol-video-ad-manager must publish the loadNext function', () => {
+test('mol-video-ad-manager must publish `loadNext`', () => {
   expect(loadNext).toBeInstanceOf(Function);
 });
 
-test('mol-video-ad-manager must publish the isAdPod function', () => {
+test('mol-video-ad-manager must publish `isAdPod`', () => {
   expect(isAdPod).toBeInstanceOf(Function);
+});
+
+test('mol-video-ad-manager must publish `createVideoAdContainer`', () => {
+  expect(createVideoAdContainer).toBeInstanceOf(Function);
 });
 
