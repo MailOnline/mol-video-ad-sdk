ae9626ead68ab7338b3624897b6cbf6ae3a2c07d
Carlos <carlos.pastor@mailonline.co.uk>
2017-11-10 15:05:46 +0000

    docs: document the ad unit emitted event
    to ease the development of the tracking logic of the adUnit
    affects: mol-video-ad-manager

--- packages/mol-video-ad-manager/src/createVideoAdUnit.js
+++ packages/mol-video-ad-manager/src/createVideoAdUnit.js
@@ -1,5 +1,35 @@
 import {createVastAdUnit} from 'mol-video-ad-unit';
 
+/*
+  LINEAR TRACKING EVENTS
+
+  * acceptInvitationLinear, <= Not used in VAST maybe for VPAID
+  * otherAdInteraction, <= Not used in VAST maybe for VPAID?
+  * timeSpentViewing, <= Not used in VAST maybe for VPAID?
+
+  EVENTS EMITTED BY VAST ADUNIT
+
+  * clickThrough, <= emit called with eventName and adUint
+  * complete,  <= emit called with eventName and adUint
+  * error, <= it will be called with an error object or undefined
+  * firstQuartile,  <= emit called with eventName and adUint
+  * iconClick, <= emit called with eventName, adUnit, iconDefinition
+  * iconView, <= emit called with eventName, adUnit, iconDefinition
+  * impression,  <= emit called with eventName and adUint
+  * midpoint,  <= emit called with eventName and adUint
+  * mute, <= emit called with eventName and adUint
+  * pause,  <= emit called with eventName and adUint
+  * playerCollapse, <= emit called with eventName and adUint
+  * playerExpand, <= emit called with eventName and adUint
+  * progress, <= emit called with eventName, adUint and {accumulated: NUMBER, contentplayhead: FORMATED_STRING}
+  * resume, <= emit called with eventName and adUint
+  * rewind, <= emit called with eventName and adUint
+  * skip, <= emit called with eventName and adUint
+  * start, <= emit called with eventName and adUint
+  * thirdQuartile,  <= emit called with eventName and adUint
+  * unmute <= emit called with eventName and adUint
+  */
+
 const createVideoAdUnit = async (vastChain, videoAdContainer, options = {}) => {
   // const {track} = options;
   const adUnit = await createVastAdUnit(vastChain, videoAdContainer, options);
