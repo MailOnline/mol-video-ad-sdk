4e5621d20e7a25363d45a006bc5f0d89e237aa8b
Carlos <carlos.pastor@mailonline.co.uk>
2017-11-13 12:08:10 +0000

    docs: remove unneeded comment
    affects: mol-video-ad-manager

--- packages/mol-video-ad-manager/src/createVideoAdUnit.js
+++ packages/mol-video-ad-manager/src/createVideoAdUnit.js
@@ -31,7 +31,6 @@ import {createVastAdUnit} from 'mol-video-ad-unit';
   */
 
 const createVideoAdUnit = async (vastChain, videoAdContainer, options = {}) => {
-  // const {track} = options;
   const adUnit = await createVastAdUnit(vastChain, videoAdContainer, options);
 
   // TODO: DO THE TRACKING OF THE ADUNIT ALLOWING AN OPTIONAL TRACK FUNCTION
