3b27cfb99a6741516b72f46ae9173f13216ab5ee
Carlos <carlos.pastor@mailonline.co.uk>
2018-02-27 15:25:27 +0000

    feat: remove onElementRemove observer
    not needed
    affects: @mol/element-observers

--- packages/element-observers/src/index.js
+++ packages/element-observers/src/index.js
@@ -1,11 +1,9 @@
 import supportsElementObservers from './supportsElementObservers';
 import onElementResize from './onElementResize';
-import onElementRemove from './onElementRemove';
 import onElementVisibilityChange from './onElementVisibilityChange';
 
 export {
   supportsElementObservers,
   onElementResize,
-  onElementRemove,
   onElementVisibilityChange
 };
