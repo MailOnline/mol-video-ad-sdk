9afc55d1abe43a55a4ab4dbdd1712a96d78e07d7
carlos <carlos.pastor@mailonline.co.uk>
2018-04-04 17:08:09 +0100

    chore: fix storybook configuration
    affects: @mol/video-ad-component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import {configure} from '@storybook/react';
 
-const req = require.context('../src', true,/(\.stories|story)\.js$/);
+const req = require.context('../packages/video-ad-component/src', true, /.*(__storybook__)\/.*(\.stories|story)\.js$/);
 
 const loadStories = () => {
   req.keys().forEach((filename) => req(filename));
