310b56e8881edd0c1c632292644cca18a33a1b9e
Va Da <streamich@gmail.com>
2018-05-30 17:39:13 +0100

    feat: 🎸 implement loading of <VideoAdIMA> comp
    affects: @mol/react-vast

---
+++ packages/react-vast/src/VideoAdIMA/VideoAdIMASync.js
@@ -0,0 +1,13 @@
+/* eslint-disable filenames/match-exported, filenames/match-regex */
+import React from 'react';
+import defaultProps from '../VideoAd/defaultProps';
+import propTypes from '../VideoAd/propTypes';
+
+export class VideoAdIMASync extends React.Component {
+  static defaultProps = defaultProps;
+  static propTypes = propTypes;
+
+  render () {
+    return <div>VideoAdIMASync</div>;
+  }
+}
--- packages/react-vast/src/VideoAdIMA/index.js
+++ packages/react-vast/src/VideoAdIMA/index.js
@@ -1,14 +1,58 @@
-/* eslint-disable filenames/match-exported */
+/* eslint-disable filenames/match-exported, react/no-did-mount-set-state */
+import {loadScript} from '@mol-fe/mol-fe-dom-helpers';
 import React from 'react';
+import timeoutPromise from '../helpers/timeoutPromise';
 import defaultProps from '../VideoAd/defaultProps';
 import propTypes from '../VideoAd/propTypes';
+
+const IMA_SDK_URL = window.IMA_SDK_URL || '//imasdk.googleapis.com/js/sdkloader/ima3_debug.js';
+const loadSDK = () => timeoutPromise(loadScript(IMA_SDK_URL), 5000);
+
+const loadComponent = async () => {
+  const {VideoAdIMASync} = await import('./VideoAdIMASync');
+
+  return VideoAdIMASync;
+};
 
 class VideoAdIMA extends React.Component {
   static defaultProps = defaultProps;
   static propTypes = propTypes;
 
+  state = {
+    Component: null,
+    error: null
+  };
+
+  async componentDidMount () {
+    try {
+      const [, Component] = await Promise.all([
+        loadSDK(),
+        loadComponent()
+      ]);
+
+      this.setState({
+        Component
+      });
+    } catch (error) {
+      this.setState({
+        error
+      });
+    }
+  }
+
   render () {
-    return <div>IMA</div>;
+    const {renderError, renderLoading, ...rest} = this.props;
+    const {Component, error} = this.state;
+
+    if (error) {
+      return null;
+    }
+
+    if (!Component) {
+      return null;
+    }
+
+    return <Component {...rest} />;
   }
 }
 
