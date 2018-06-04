bcf99d9e882d5d57937b6f71ed6cead2b48ce214
Va Da <streamich@gmail.com>
2018-06-04 11:03:03 +0100

    fix: 🐜 make IMA ad component "work"
    affects: @mol/react-vast

--- packages/react-vast/src/VideoAdIMA/VideoAdIMASync.js
+++ packages/react-vast/src/VideoAdIMA/VideoAdIMASync.js
@@ -9,6 +9,9 @@ export class VideoAdIMASync extends React.Component {
   static propTypes = propTypes;
 
   componentDidMount () {
+    const ima = window.google.ima;
+
+    // TODO: GET VIDEO ELEMENT. DO WE NEED AN ACTUAL VIDEO ELEMENT?
     const videoElement = undefined;
 
     this.adDisplayContainer = new window.google.ima.AdDisplayContainer(this.el, videoElement);
@@ -16,58 +19,68 @@ export class VideoAdIMASync extends React.Component {
     // NOTE: Must be done as the result of a user action on mobile
     this.adDisplayContainer.initialize();
 
-    const adsLoader = getAdsLoader(this.adDisplaadDisplayContainer);
+    this.adsLoader = getAdsLoader(this.el);
 
     // Add event listeners
-    adsLoader.addEventListener(
-    google.ima.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED,
-    onAdsManagerLoaded,
-    false);
-    adsLoader.addEventListener(
-    google.ima.AdErrorEvent.Type.AD_ERROR,
-    onAdError,
-    false);
-
-    function onAdError(adErrorEvent) {
-    // Handle the error logging and destroy the AdsManager
-    console.log(adErrorEvent.getError());
-    adsManager.destroy();
-    }
-
-    // An event listener to tell the SDK that our content video
-    // is completed so the SDK can play any post-roll ads.
-    var contentEndedListener = function() {adsLoader.contentComplete();};
-    videoContent.onended = contentEndedListener;
+    this.adsLoader.addEventListener(ima.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED, this.onAdsManagerLoaded, false);
+    this.adsLoader.addEventListener(ima.AdErrorEvent.Type.AD_ERROR, this.onAdError, false);
 
     // Request video ads.
-    var adsRequest = new google.ima.AdsRequest();
-    adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
-    'sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&' +
-    'impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&' +
-    'cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
+    this.adsRequest = new ima.AdsRequest();
 
-    // Specify the linear and nonlinear slot sizes. This helps the SDK to
-    // select the correct creative if multiple are returned.
-    adsRequest.linearAdSlotWidth = 640;
-    adsRequest.linearAdSlotHeight = 400;
-    adsRequest.nonLinearAdSlotWidth = 640;
-    adsRequest.nonLinearAdSlotHeight = 150;
+    this.adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
+      'sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&' +
+      'impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&' +
+      'cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
 
-    var playButton = document.getElementById('playButton');
-    playButton.addEventListener('click', requestAds);
-
-    function requestAds() {
-    adsLoader.requestAds(adsRequest);
-    }
+    // TODO: USE VIDEO PLAYER DIMENSIONS?
+    this.adsRequest.linearAdSlotWidth = 300;
+    this.adsRequest.linearAdSlotHeight = 300;
+    this.adsRequest.nonLinearAdSlotWidth = 300;
+    this.adsRequest.nonLinearAdSlotHeight = 300;
   }
 
   el = null;
+  adsManager = null;
+  adsLoader = null;
+  adsRequest = null;
 
   ref = (el) => {
     this.el = el;
   };
 
+  onIMAError = (adErrorEvent) => {
+    const error = adErrorEvent.getError();
+
+    console.log('onIMAERROR', error);
+    if (this.adsManager) {
+      this.adsManager.destroy();
+    }
+  };
+
+  onAdError = () => {
+    console.log('onAdError');
+  };
+
+  onAdsManagerLoaded = () => {
+    console.log('onAdsManagerLoaded()');
+  };
+
+  // An event listener to tell the SDK that our content video
+  // is completed so the SDK can play any post-roll ads.
+  contentEndedListener = () => {
+    if (this.adsLoader) {
+      this.adsLoader.contentComplete();
+    }
+  };
+
+  // In the example it is triggered by a play button click.
+  requestAds () {
+    this.adsLoader.requestAds(this.adsRequest);
+  }
+
   render () {
+    console.log('render()');
     return (
       <div
         ref={this.ref}
