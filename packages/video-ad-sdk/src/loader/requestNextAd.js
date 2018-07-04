df9423f94d6e2450ff2e4d2c886d0cbfac7d99c4
carlos <carlos.pastor@mailonline.co.uk>
2018-07-04 14:30:10 +0200

    docs: ✏️ add jsdoc
    affects: @mol/video-ad-sdk

--- packages/video-ad-sdk/src/loader/requestNextAd.js
+++ packages/video-ad-sdk/src/loader/requestNextAd.js
@@ -16,6 +16,30 @@ const validateChain = (VASTChain) => {
   }
 };
 
+/**
+ * @function requestNextAd
+ * @static
+ * @description Requests the next ad in the VAST Chain.
+ *
+ * @param {VASTChain} VASTChain - Array of VAST responses. See requestAd for more info.
+ * @param {Object} options - Options Map. The allowed properties are:
+ * @param {number} [options.wrapperLimit] - Sets the maximum number of wrappers allowed in the vastChain.
+ *  Defaults to `5`.
+ * @param {boolean} [options.AllowMultipleAds] - Boolean to indicate whether adPods are allowed or not.
+ *  Defaults to `true`.
+ * @param {tracker} [options.track] - optional function to track whatever errors occur during the loading.
+ *  Defaults to `@mol/video-ad-tracker` track method.
+ * @param {boolean} [options.useAdBuffet] - Specifies whether to use buffet ads from an ad pod if possible.
+ *    If no buffet ad is available it will return the next ad in ad pod sequence.
+ *    Set it to true if an ad from an adPod failed and you want to replace it with an ad from the ad buffet.
+ *    Defaults to `false`.
+ * @param {boolean} [options.fallbackOnNoAd] - tells the video player to select an from any stand-alone ads available.
+ *    Note: if the {@link VASTChain} contains an adPod this property will be ignored.
+ *    Defaults to `true`.
+ *
+ * @returns Promise<VASTChain>  - Returns a Promise that will resolve a VastChain with the newest VAST response at the begining of the array.
+ * If the VastChain had an error. The first VAST response of the array will contain an error and an errorCode entry.
+ */
 const requestNextAd = (VASTChain, options) => {
   validateChain(VASTChain);
 
