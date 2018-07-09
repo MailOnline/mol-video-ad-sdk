d7ab6ff0fe58bd0a0308792324adfe4b41463e48
carlos <carlos.pastor@mailonline.co.uk>
2018-07-09 10:56:40 +0200

    feat: 🎸 add VpaidAdUnit scheleton
    affects: @mol/video-ad-sdk

---
+++ packages/video-ad-sdk/src/adUnit/VpaidAdUnit.js
@@ -0,0 +1,54 @@
+/* eslint-disable promise/prefer-await-to-callbacks, class-methods-use-this */
+import Emitter from './helpers/Emitter';
+
+class VpaidAdUnit extends Emitter {
+  constructor (vastChain, videoAdContainer, {logger = console} = {}) {
+    super(logger);
+  }
+
+  async start () {
+
+  }
+
+  resume () {
+
+  }
+
+  pause () {
+
+  }
+
+  setVolume () {
+
+  }
+
+  getVolume () {
+
+  }
+
+  cancel () {
+
+  }
+
+  onFinish () {
+
+  }
+
+  onError () {
+
+  }
+
+  isFinished () {
+
+  }
+
+  isStarted () {
+
+  }
+
+  async resize () {
+
+  }
+}
+
+export default VpaidAdUnit;
