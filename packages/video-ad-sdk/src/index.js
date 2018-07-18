88546fce58a2ae4a3d89e609458dbe91c975632e
carlos <carlos.pastor@mailonline.co.uk>
2018-07-18 14:40:10 +0200

    feat: 🎸 export VpaidAdUnit
    affects: @mol/video-ad-sdk

--- packages/video-ad-sdk/src/index.js
+++ packages/video-ad-sdk/src/index.js
@@ -1,7 +1,12 @@
-/** @module @mol/video-ad-sdk */
+/**
+ * @module @mol/video-ad-sdk
+ * @description Video ad SDK to load and play HTML5 video ads.
+ *
+ */
 
 import VideoAdContainer from './adContainer/VideoAdContainer';
 import VastAdUnit from './adUnit/VastAdUnit';
+import VpaidAdUnit from './adUnit/VpaidAdUnit';
 import run from './runner/run';
 import runWaterfall from './runner/runWaterfall';
 import requestAd from './vastRequest/requestAd';
@@ -9,6 +14,7 @@ import requestNextAd from './vastRequest/requestNextAd';
 
 export {
   VastAdUnit,
+  VpaidAdUnit,
   VideoAdContainer,
   run,
   runWaterfall,
