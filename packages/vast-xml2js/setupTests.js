a82a7ca77d88f2914447327c0ddcf71bc15c6c9e
carlos <carlos.pastor@mailonline.co.uk>
2018-07-31 10:40:40 +0200

    chore: 🚜 simplify jest config
    affects: @mol/vast-xml2js
    
    Issues: #60

--- packages/vast-xml2js/setupTests.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-unassigned-import
-import 'raf-polyfill';
-
-const noop = () => {};
-
-// JSDOM throws a 'Not implemented' error if you call this methods
-HTMLMediaElement.prototype.play = noop;
-HTMLMediaElement.prototype.pause = noop;
